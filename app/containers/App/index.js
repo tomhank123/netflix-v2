6f2ec99430054b98320dac2f73206a68a7d31594
Minh Duc <ductm@devblock.net>
2021-05-24 12:46:32 +0700

    chore: install bootstrap framework

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,6 +9,7 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import HomePage from 'containers/HomePage/Loadable';
 import MoviesHome from 'containers/MoviesHome/Loadable';
