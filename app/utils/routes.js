de1d6392dba9eebd1ba59bf646f0b56e5125f0d6
Minh Duc <ductm@devblock.net>
2021-05-28 16:48:51 +0700

    feat: create auth routes

--- app/utils/routes.js
+++ app/utils/routes.js
@@ -1,13 +1,67 @@
-/* eslint-disable prettier/prettier */
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
 export const HOME = '/';
 export const BROWSE = '/browse';
 export const WATCH = '/watch';
 export const LATEST = '/latest';
-export const MY_LIST = '/my-list';
 export const SEARCH = '/search';
-
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 
-export const MOVIES = '/browse/genre/:genreId';
-export const SERIES = '/browse/genre/:genreId';
+export function IsUserRedirect({ user, loggedInPath, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return <Redirect to={{ pathname: loggedInPath }} />;
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+IsUserRedirect.propTypes = {
+  user: PropTypes.object,
+  loggedInPath: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export function ProtectedRoute({ user, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: 'signin',
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+ProtectedRoute.propTypes = {
+  user: PropTypes.object,
+  children: PropTypes.node,
+};
