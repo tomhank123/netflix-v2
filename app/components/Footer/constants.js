8c1a0efd3edc9d909bdf5f32c572ca1d4b9a864b
Duc Tran Minh <ductm@devblock.net>
2021-06-07 16:55:17 +0700

    refactor: update footer

---
+++ app/components/Footer/constants.js
@@ -0,0 +1,67 @@
+export const footerLinks = [
+  {
+    id: 0,
+    title: 'Audio and Subtitles',
+    url: '/audio-and-subtitles',
+  },
+  {
+    id: 1,
+    title: 'Audio Description',
+    url: '/audio-description',
+  },
+  {
+    id: 2,
+    title: 'Help Center',
+    url: '/help-center',
+  },
+  {
+    id: 3,
+    title: 'Gift Cards',
+    url: '/gift-cards',
+  },
+  {
+    id: 4,
+    title: 'Media Center',
+    url: '/media-center',
+  },
+  {
+    id: 5,
+    title: 'Investor Relations',
+    url: '/investor-relations',
+  },
+  {
+    id: 6,
+    title: 'Jobs',
+    url: '/jobs',
+  },
+  {
+    id: 7,
+    title: 'Terms of Use',
+    url: '/terms-of-use',
+  },
+  {
+    id: 8,
+    title: 'Privacy',
+    url: '/privacy',
+  },
+  {
+    id: 9,
+    title: 'Legal Notices',
+    url: '/legal-notices',
+  },
+  {
+    id: 10,
+    title: 'Cookie Preferences',
+    url: '/cookie-preferences',
+  },
+  {
+    id: 11,
+    title: 'Corporate Information',
+    url: '/corporate-information',
+  },
+  {
+    id: 12,
+    title: 'Contact Us',
+    url: '/contact-us',
+  },
+];
--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -5,84 +5,17 @@
  */
 
 import React from 'react';
-
 import { Button, Container } from 'react-bootstrap';
 import {
   FaFacebookSquare,
+  FaInstagram,
   FaTwitter,
   FaYoutube,
-  FaInstagram,
 } from 'react-icons/fa';
-import Wrapper from './Wrapper';
+import { footerLinks } from './constants';
 import Nav from './Nav';
+import Wrapper from './Wrapper';
 
-const footerLinks = [
-  {
-    id: 0,
-    title: 'Audio and Subtitles',
-    url: '/audio-and-subtitles',
-  },
-  {
-    id: 1,
-    title: 'Audio Description',
-    url: '/audio-description',
-  },
-  {
-    id: 2,
-    title: 'Help Center',
-    url: '/help-center',
-  },
-  {
-    id: 3,
-    title: 'Gift Cards',
-    url: '/gift-cards',
-  },
-  {
-    id: 4,
-    title: 'Media Center',
-    url: '/media-center',
-  },
-  {
-    id: 5,
-    title: 'Investor Relations',
-    url: '/investor-relations',
-  },
-  {
-    id: 6,
-    title: 'Jobs',
-    url: '/jobs',
-  },
-  {
-    id: 7,
-    title: 'Terms of Use',
-    url: '/terms-of-use',
-  },
-  {
-    id: 8,
-    title: 'Privacy',
-    url: '/privacy',
-  },
-  {
-    id: 9,
-    title: 'Legal Notices',
-    url: '/legal-notices',
-  },
-  {
-    id: 10,
-    title: 'Cookie Preferences',
-    url: '/cookie-preferences',
-  },
-  {
-    id: 11,
-    title: 'Corporate Information',
-    url: '/corporate-information',
-  },
-  {
-    id: 12,
-    title: 'Contact Us',
-    url: '/contact-us',
-  },
-];
 function Footer() {
   return (
     <Wrapper>
